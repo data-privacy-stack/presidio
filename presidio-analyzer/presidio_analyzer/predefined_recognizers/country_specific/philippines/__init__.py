fb624b2fb3c0944fd79a2a2799a00f9c89c26b44
Omri Mendels <omri374@users.noreply.github.com>
2026-07-26 16:14:21 +0300

    fixed __all__

--- presidio-analyzer/presidio_analyzer/predefined_recognizers/country_specific/philippines/__init__.py
+++ presidio-analyzer/presidio_analyzer/predefined_recognizers/country_specific/philippines/__init__.py
@@ -5,7 +5,8 @@ from .ph_tin_recognizer import PhTinRecognizer
 from .ph_umid_recognizer import PhUmidRecognizer
 
 __all__ = [
-    "PhPassportRecognizer", "PhTinRecognizer"
+    "PhPassportRecognizer",
+    "PhTinRecognizer",
     "PhTinRecognizer",
     "PhUmidRecognizer",
 ]
